511589b65a5cecb843d539fab663459ccbf30e10
bikeman Wang <bikeman18@hotmail.com>
2016-06-06 07:05:50 +0000

    add user_agent detect in devise_failure

--- lib/devise_failure.rb
+++ lib/devise_failure.rb
@@ -1,5 +1,9 @@
 class DeviseFailure < Devise::FailureApp
   def redirect_url
-    user_wechat_omniauth_authorize_path
+    if request.user_agent.match(/micromessenger/i)
+      user_wechat_omniauth_authorize_path
+    else
+      new_user_session_path
+    end
   end
 end
